cb2e97ab6b0ca674d100030aca1b0845ee43d951
Khashayar Haghighi <xerxes.2000a@gmail.com>
2021-12-09 23:47:09 +0330

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,34 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    int[] denom = {1,4,6};
+	    
+		System.out.println(minChange(denom,9));
+	}
+	
+public static int minChange(int[] denom, int targetAmount) {
+
+    int actualAmount;
+    int m = denom.length+1;
+    int n = targetAmount + 1;
+    int inf = Integer.MAX_VALUE-1;
+
+    int[][] table = new int[m][n];
+    for (int j = 1; j < n; j++)
+        table[0][j] = inf;
+
+    for (int denomPosition = 1; denomPosition < m; denomPosition++) {
+        for (int currentAmount = 1; currentAmount < n; currentAmount++) {
+            if (currentAmount - denom[denomPosition-1] >= 0)
+                actualAmount = table[denomPosition][currentAmount - denom[denomPosition-1]];
+            else
+                actualAmount = inf;
+            table[denomPosition][currentAmount] = Math.min(table[denomPosition-1][currentAmount], 1 + actualAmount);
+        }
+    }
+
+    return table[m-1][n-1];
+
+}
+}
